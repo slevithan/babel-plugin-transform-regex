5102a02250f09a1c908f3de04d4c44d09b24fc3a
Steven Levithan <steves_list@hotmail.com>
2024-07-07 06:29:47 +0200

    Add demo, support for interpolation, etc.

--- README.md
+++ README.md
@@ -1,28 +1,62 @@
 # babel-plugin-transform-regex
 
-Transpiles tagged [`regex`](https://github.com/slevithan/regex) template literals into native `RegExp` literals, enabling syntax for modern and more readable regex features (atomic groups, subroutines, insignificant whitespace, line comments, *named capture only* mode, etc.), without the need for calling `regex` at runtime.
+This is a [Babel](https://babel.dev/) plugin that transpiles tagged [`regex`](https://github.com/slevithan/regex) template literals into native `RegExp` literals, enabling syntax for modern and more readable regex features (atomic groups, subroutines, insignificant whitespace, comments, *named capture only* mode, etc.) without the need for calling `regex` at runtime. Although `regex` is already a lightweight and high-performance package, nevertheless this takes things even further by giving you its developer experience benefits while allowing you to avoid adding any runtime dependencies and without users paying any runtime cost.
 
-Currently, this only transforms tagged `regex` templates that **don't use interpolation**.
-
-The following call formats are currently supported:
+The following call formats are all supported:
 
 - `` regex`<pattern>` ``
 - `` regex()`<pattern>` ``
 - `` regex('<flags>')`<pattern>` ``
-- `` regex({flags: '<flags>'})`<pattern>` ``
-- `regex({raw: ['<pattern>']})` - Called as a function instead of with backticks.
+- `` regex({<options>})`<pattern>` ``
 
-**TODO:** Support additional usage patterns:
+Interpolation into the regex pattern is supported, so long as the interpolated values are:
 
-- Interpolation of non-dynamic literals (strings, regexes, etc.).
-- Interpolation of regexes and partials constructed with non-dynamic literals.
-- Additional edge case call formats.
+- Inline string, regex, or number literals.
+- Inline regexes constructed via `RegExp` with string values.
+- Inline partials. Allows both `` partial`…` `` as a template tag (without interpolation) and `partial(…)` with a string or number value.
 
-<!-- Examples of edge case call formats:
-- `` regex(`<flags>`)`<pattern>` ``, without interpolation of flags or with only literal strings interpolated
-- `regex('<flags>')({raw: ['<pattern>']})`
-- `regex({flags: '<flags>'})({raw: ['<pattern>']})`
-- `regex({raw: [<strs>]}, <values>)`, with literal strs and values 
--->
+Additional details:
 
-**NOTE:** Outputted regexes use flag `v`, supported by Node.js 20+ and 2023-era browsers or later. You can further transpile away the `v` flag with Babel's official plugin [@babel/plugin-transform-unicode-sets-regex](https://babel.dev/docs/babel-plugin-transform-unicode-sets-regex) that is also included in `@babel/preset-env`.
+- Wherever strings are allowed, `'…'`, `"…"`, `` `…` ``, and `` String.raw`…` `` can all be used, so long as they don't include interpolation.
+- Tagged `regex` templates that interpolate variables or other dynamic values are **not transformed**.
+- Basic support is included for transforming the `regex` tag when called as a function instead of with backticks, via `regex({raw: ['<pattern>']})`.
+
+**TODO:** Support for additional usage patterns (including e.g. interpolating *variables* that hold non-dynamic strings, regexes, numbers, and partials) might be added in future versions. Contributions are welcome!
+
+## Compatibility
+
+Outputted regexes use flag `v`, supported by Node.js 20+ and 2023-era browsers or later. You can further transpile away the `v` flag with Babel's official plugin [@babel/plugin-transform-unicode-sets-regex](https://babel.dev/docs/babel-plugin-transform-unicode-sets-regex) that is included in [@babel/preset-env](https://babel.dev/docs/babel-preset-env).
+
+## Installation and usage
+
+Install a recent version of Babel, and this plugin:
+
+```sh
+npm install --save-dev @babel/core @babel/cli
+npm install --save-dev babel-plugin-transform-regex
+```
+Run the following command to compile all your code from the `src` directory to `lib`:
+
+```sh
+./node_modules/.bin/babel src --out-dir lib --plugins=babel-plugin-transform-regex
+```
+
+### Optional setup steps
+
+To make this easier to use, you can create a config file in the root of your project named `babel.config.json`, with this content:
+
+```json
+{
+  "plugins": ["babel-plugin-transform-regex"]
+}
+```
+
+Then add the following script to your `package.json`:
+
+```json
+"scripts": {
+  "build": "babel src --out-dir lib"
+}
+```
+
+After that, you can run it via `npm run build`.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,142 @@
-import {regex} from 'regex';
+import {regex, partial} from 'regex';
 
 export default ({types: t}) => {
+  function isNondynamicRegExpCall(node) {
+    // `RegExp` can be called with or without `new`
+    if (!t.isNewExpression(node) && !t.isCallExpression(node)) {
+      return false;
+    }
+    const args = node.arguments;
+    return t.isIdentifier(node.callee, {name: 'RegExp'}) &&
+      (args.length === 1 || args.length === 2) &&
+      args.every(a => isNondynamicString(a));
+  }
+  // Assumes the structure was validated by `isNondynamicRegExpCall`
+  function getNondynamicRegExpCall(node) {
+    const args = node.arguments;
+    return new RegExp(getNondynamicString(args[0]), getNondynamicString(args[1]));
+  }
+  function isNondynamicPartial(node) {
+    return ( // `partial` call
+      t.isCallExpression(node) &&
+      t.isIdentifier(node.callee, {name: 'partial'}) &&
+      node.arguments.length === 1 &&
+      (isNondynamicString(node.arguments[0]) || t.isNumericLiteral(node.arguments[0]))
+    ) ||
+    ( // `partial` template tag, without interpolation
+      t.isTaggedTemplateExpression(node) &&
+      t.isIdentifier(node.tag, {name: 'partial'}) &&
+      node.quasi.quasis.length === 1
+    );
+  }
+  // Assumes the structure was validated by `isNondynamicPartial`
+  function getNondynamicPartial(node) {
+    if (t.isCallExpression(node)) {
+      const arg = node.arguments[0];
+      if (isNondynamicString(arg)) {
+        return partial(getNondynamicString(arg));
+      }
+      if (t.isNumericLiteral(arg)) {
+        return partial(arg.value);
+      }
+    } else if (t.isTaggedTemplateExpression(node)) {
+      return partial(node.quasi.quasis[0].value.raw);
+    }
+  }
+  function isNondynamicString(node) {
+    return t.isStringLiteral(node) ||
+      ( // Allow: `...`, without interpolation
+        t.isTemplateLiteral(node) &&
+        node.quasis.length === 1
+      ) ||
+      ( // Allow: String.raw`...`, without interpolation
+        t.isTaggedTemplateExpression(node) &&
+        node.quasi.quasis.length === 1 &&
+        t.isMemberExpression(node.tag) &&
+        t.isIdentifier(node.tag.object, {name: 'String'}) &&
+        t.isIdentifier(node.tag.property, {name: 'raw'})
+      );
+  }
+  // Assumes the structure was validated by `isNondynamicString`
+  function getNondynamicString(node) {
+    if (t.isStringLiteral(node)) {
+      return node.value;
+    }
+    if (t.isTemplateLiteral(node)) {
+      return node.quasis[0].value.cooked;
+    }
+    if (t.isTaggedTemplateExpression(node)) {
+      return node.quasi.quasis[0].value.raw;
+    }
+  }
+  function isSimpleOptionsObject(node) {
+    return t.isObjectExpression(node) &&
+      node.properties.every(p => {
+        return t.isObjectProperty(p) &&
+          t.isIdentifier(p.key) &&
+          (isNondynamicString(p.value) || t.isBooleanLiteral(p.value));
+      });
+  }
+  // Assumes the structure was validated by `isSimpleOptionsObject`
+  function getSimpleOptionsObject(node) {
+    const object = {};
+    node.properties.forEach(p => {
+      object[p.key.name] = isNondynamicString(p.value) ?
+        getNondynamicString(p.value) :
+        p.value.value;
+    });
+    return object;
+  }
+  function isWhitelistedInterpolation(expressions) {
+    return expressions.every(e => {
+      return isNondynamicString(e) ||
+        t.isNumericLiteral(e) ||
+        t.isRegExpLiteral(e) ||
+        isNondynamicRegExpCall(e) ||
+        isNondynamicPartial(e);
+    });
+  }
+  function getRegexCallArg(node) {
+    const args = node.tag.arguments ?? [];
+    if (args.length) {
+      const arg = args[0];
+      if (isNondynamicString(arg)) {
+        return getNondynamicString(arg);
+      } else if (isSimpleOptionsObject(arg)) {
+        return getSimpleOptionsObject(arg);
+      }
+    }
+  }
+  function getRegexQuasisRaw(node) {
+    const result = [];
+    node.quasi.quasis.forEach(q => result.push(q.value.raw));
+    return result;
+  }
+  function getRegexExpressions(node) {
+    const result = [];
+    node.quasi.expressions.forEach(e => {
+      // These checks mirror `isWhitelistedInterpolation`
+      if (isNondynamicString(e)) {
+        result.push(getNondynamicString(e));
+      } else if (t.isNumericLiteral(e)) {
+        result.push(e.value);
+      } else if (t.isRegExpLiteral(e)) {
+        result.push(new RegExp(e.pattern, e.flags));
+      } else if (isNondynamicRegExpCall(e)) {
+        result.push(getNondynamicRegExpCall(e));
+      } else if (isNondynamicPartial(e)) {
+        result.push(getNondynamicPartial(e));
+      }
+    });
+    return result;
+  }
+
   function isRegexTemplate(node) {
-    // Don't allow using interpolation
+    // Restrict interpolation into the pattern to non-dynamic string/number/regexp literals, plus
+    // regexes and partials constructed with non-dynamic literals
     if (!(
       t.isTemplateLiteral(node.quasi) &&
-      node.quasi.quasis.length === 1
+      (node.quasi.quasis.length === 1 || isWhitelistedInterpolation(node.quasi.expressions))
     )) {
       return false;
     }
@@ -30,21 +161,14 @@ export default ({types: t}) => {
     }
     const arg = node.tag.arguments[0];
     // Allow: regex('<flags>')`<pattern>`
-    if (t.isStringLiteral(arg)) {
-      return true;
-    }
-    // Allow: regex({flags: '<flags>'})`<pattern>`
-    if (
-      t.isObjectExpression(arg) &&
-      arg.properties.length === 1 &&
-      t.isObjectProperty(arg.properties[0]) &&
-      t.isIdentifier(arg.properties[0].key, {name: 'flags'})
-    ) {
+    // Allow: regex({<options>})`<pattern>`
+    if (isNondynamicString(arg) || isSimpleOptionsObject(arg)) {
       return true;
     }
     return false;
   }
-  function isRegexTemplateViaRawProp(node) {
+
+  function isRegexTemplateViaCall(node) {
     if (!(
       t.isIdentifier(node.callee, {name: 'regex'}) &&
       node.arguments.length === 1
@@ -64,23 +188,7 @@ export default ({types: t}) => {
     ) {
       return true;
     }
-    // TODO: Allow: `regex(...)({raw: ['<pattern>']})`, with flags
     return false;
-  }
-  function getFlags(node) {
-    if (!t.isCallExpression(node.tag) || !node.tag.arguments.length) {
-      return;
-    }
-    const arg = node.tag.arguments[0];
-    if (t.isStringLiteral(arg)) {
-      return arg.value;
-    }
-    if (t.isObjectExpression(arg)) {
-      // const flagsProp = arg.properties.find(obj => obj.key.name === 'flags');
-      // return flagsProp?.value.value;
-      return arg.properties[0].value.value;
-    }
-    return;
   }
 
   return {
@@ -89,16 +197,24 @@ export default ({types: t}) => {
         if (!isRegexTemplate(path.node)) {
           return;
         }
-        const pattern = path.node.quasi.quasis[0].value.raw;
-        const re = regex(getFlags(path.node))({raw: [pattern]});
+        const callArg = getRegexCallArg(path.node);
+        const quasis = getRegexQuasisRaw(path.node);
+        const expressions = getRegexExpressions(path.node);
+        const re = regex(callArg)({raw: quasis}, ...expressions);
         path.replaceWith(t.regExpLiteral(re.source, re.flags));
       },
       CallExpression(path) {
-        if (!isRegexTemplateViaRawProp(path.node)) {
+        // Currently only has basic support for `regex({raw: ['<pattern>']})`
+        // TODO: Allow:
+        // - `regex('<flags>')({raw: ['<pattern>']})`
+        // - `regex({<options>})({raw: ['<pattern>']})`
+        // - `regex({raw: [`<pattern>`]})`, with template literal or String.raw`<pattern>`
+        // - `regex({raw: ['<pattern>', '<pattern>']}, <value>)`, with template values
+        if (!isRegexTemplateViaCall(path.node)) {
           return;
         }
         const pattern = path.node.arguments[0].properties[0].value.elements[0].value;
-        const re = regex()({raw: [pattern]});
+        const re = regex({raw: [pattern]});
         path.replaceWith(t.regExpLiteral(re.source, re.flags));
       },
     },
